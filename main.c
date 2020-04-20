403e4900189569cd695fa7535b653a985cd6d413
chirag <arorachirag155@gmail.com>
2020-04-21 03:03:32 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ struct noded
     struct node* prev;
 };
 struct node* head=NULL;
+struct noded* headed=NULL;
 
 int main()
 {
@@ -95,7 +96,7 @@ void insert_end()
 }
 void delete_front()
 {
-    struct node *temp,*p;
+    struct node *temp;
     if(head->next==NULL)
     {
         free(head);
@@ -104,7 +105,7 @@ void delete_front()
     {
         temp=head;
         free(temp);
-        p->next=head;
+        head=temp->next;
     }
 }
 void delete_between()
@@ -125,9 +126,8 @@ void delete_between()
         {
             p=p->next;
         }
-        p->next=temp;
+        temp=p->next;
         p->next=temp->next;
-        temp->next=NULL;
         free(temp);
     }
 }
@@ -146,12 +146,86 @@ void delete_end()
             p=p->next;
         }
         free(p->next);
-        p->next=NULL;
+
     }
 }
-void double_front()
+void double_front_insert()
 {
+    struct noded* temp;
+    int num;
+    printf("\nenter the data ");
+    scanf("%d",&num);
+    temp=(struct noded*)malloc(sizeof(struct noded));
+        temp->data=num;
 
+    if (headed==NULL)
+    {
+        temp->next=NULL;
+        temp->prev=NULL;
+        headed=temp;
+    }
+    else
+    {
+        temp->next=headed;
+        temp->prev=NULL;
+        headed->prev=temp;
+        headed=temp;
+    }
+}
+void double_end_insert()
+{
+    struct noded* temp,*p;
+    int num;
+    printf("\nenter the data ");
+    scanf("%d",&num);
+    temp=(struct noded*)malloc(sizeof(struct noded));
+        temp->data=num;
+
+    if (headed==NULL)
+    {
+        temp->next=NULL;
+        temp->prev=NULL;
+        headed=temp;
+    }
+    else
+    {
+        p=headed;
+        while(p->next!=NULL)
+        {
+            p=p->next;
+        }
+        p->next=temp;
+        temp->prev=p;
+        temp->next=NULL;
+
+    }
+}
+void double_mid_insert()
+{
+    struct noded* temp,*p;
+    int num,pos;
+    printf("\nenter the position ");
+    scanf("%d",&pos);
+    printf("\nenter the data ");
+    scanf("%d",&num);
+    temp=(struct noded*)malloc(sizeof(struct noded));
+    temp->data=num;
+    p=headed;
+    if (headed==NULL)
+    {
+        headed=temp;
+    }
+    else
+    {
+         for(int i=0;i<pos;i++)
+        {
+            p=p->next;
+        }
+        temp->next=p->next;
+        temp->prev=p;
+        p->next=temp;
+        temp->next->prev=temp;
+    }
 }
 void display()
 {
@@ -159,7 +233,96 @@ void display()
     p=head;
     while(p!=NULL)
     {
-        printf("%d-->",p->data);
+        printf("%d",p->data);
         p=p->next;
     }
 }
+void double_display()
+{
+    struct node *p;
+    p=headed;
+    while(p!=NULL)
+    {
+        printf("%d",p->data);
+        p=p->next;
+    }
+}
+void circular_insert_front()
+{
+    struct node* temp,*p;
+    int num;
+    printf("\nenter the data ");
+    scanf("%d",&num);
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=num;
+
+    if (head==NULL)
+    {
+        temp=temp->next;
+        head=temp;
+    }
+    else
+    {
+        p=head;
+        while(p->next!=head)
+        {
+            p=p->next;
+        }
+        temp=p->next;
+        head=temp->next;
+    }
+}
+void circular_insert_mid()
+{
+    struct node *temp,*p;
+    int i,num,pos;
+    printf("\nenter the data");
+    scanf("%d",&num);
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=num;
+    if(head=NULL)
+    {
+        temp->next=head;
+        head=temp;
+    }
+    else
+    {
+        p=head;
+        while( i<pos)
+        {
+            p=p->next;
+            i++;
+        }
+        temp->next=p->next;
+        p->next=temp;
+
+    }
+
+
+}
+void circular_insert_end()
+{
+    struct node* temp,*p;
+    int num;
+    printf("\nenter the data ");
+    scanf("%d",&num);
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=num;
+
+    if (head==NULL)
+    {
+        temp->next=temp;
+        head=temp;
+    }
+    else
+    {
+        p=head;
+        while(p->next!=head)
+        {
+            p=p->next;
+        }
+        head=temp->next;
+        p->next=temp;
+    }
+}
+
